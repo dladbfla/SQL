dd50b623f8295626c147d53163c7d148f10ea6e7
dladbfla <161662853+dladbfla@users.noreply.github.com>
2024-04-15 17:33:50 +0900

    4.15

---
+++ Datatype.sql
@@ -0,0 +1,32 @@
+/* 문자형데이터타입
+char 고정길이 255
+varchar 가변길이 65,535
+tinytext 255
+text 65,535
+mediumtext 16,777,215
+longtext 4,294,967,295
+json 1기가 */
+
+/* 숫자형 데이터타입
+tinyint 1byte
+smallint 2byte
+mediumint 3byte
+int 4byte
+bigint 8byte
+
+float 4byte(소수점아래 7자리)
+double 8byte(소수점아래 15자리)
+decimal(전체자릿수, 소수점자릿수) */
+
+/* 날짜시간형 데이터타입
+date YYYY-MM-DD
+time HH:MI:SS
+datetime YYYY-MM-DD HH:MI:SS
+timestamp YYYY-MM-DD HH:MI:SS */
+
+/* 이진형(binary) 데이터타입 : 이미지, 오디오, 비디오등
+tinyblob 255byte
+blob 65535byte (blob = binary large object)
+medium blob 16,777,215byte
+longblob 4,294,967,295byte
+ */
